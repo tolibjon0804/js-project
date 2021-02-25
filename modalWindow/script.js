59de0f9d1ecb54a694e583d8b75c02aced3dfc00
Tolibjon Olimjonov <72200127+tolibjon0804@users.noreply.github.com>
2021-02-25 13:04:31 +0500

    Add files via upload

---
+++ modalWindow/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+btnsOpenModal.forEach((item) => {
+  item.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
